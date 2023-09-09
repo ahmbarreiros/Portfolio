a8400e9682a0c2d0306172c24aded8918586cace
Antonio <ahmbarreiros.cs@gmail.com>
2023-09-09 18:53:51 -0300

    Header

---
+++ src/About.jsx
@@ -0,0 +1,11 @@
+const About = () => {
+    return (
+        <section className="about">
+            <h1 className="about-name">
+                #define <b className="about-name-macro">MY_NAME</b>{" "}
+                <span className="about-name-myName">"Antonio Barreiros"</span>;
+            </h1>
+        </section>
+    );
+};
+export default About;
