9963c438351886a269a6eb914134bd973ae724bb
Antonio <ahmbarreiros.cs@gmail.com>
2023-09-09 18:53:42 -0300

    Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
+// import "./App.css";
+import Header from "./Header";
+import About from "./About";
 
 function App() {
     return (
         <>
-            <h1>Hi</h1>
+            <Header />
+            <main>
+                <About />
+            </main>
         </>
     );
 }
